b39585450f79109c7ed86b68ed5489fb0782cca6
Konstantinos Despoinidis <kdesp2003@gmail.com>
2024-06-28 00:22:34 +0300

    changed version

--- include/webc-core.h
+++ include/webc-core.h
@@ -32,8 +32,8 @@
 
 #define VERSION_MAJOR 0
 #define VERSION_MINOR 0
-#define VERSION_PATCH 5
-#define VERSION "0.0.5"
+#define VERSION_PATCH 6
+#define VERSION "0.0.6"
 
 #ifndef WEBCAPI
     #define WEBCAPI extern // Functions defined as 'extern' by default (implicit specifiers)
